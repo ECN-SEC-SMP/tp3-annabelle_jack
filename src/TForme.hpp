51a657b368b9467875b8d166230454a7a11f57b8
Annabelle <annabelle.goter@eleves.ec-nantes.fr>
2025-11-17 17:25:43 +0100

    TForme finito

---
+++ src/TForme.hpp
@@ -0,0 +1,35 @@
+#ifndef _TFORME_HPP
+#define _TFORME_HPP
+
+#include "TPoint.hpp"
+
+template <typename T>
+class TForme {
+    private :
+    TPoint<T> centre;
+
+    public :
+    TForme(void){
+        this->centre = TPoint();
+    }
+
+    TForme(TPoint<T> point){
+        this->centre = point;
+    }
+
+    // Methods virtuals //
+    virtual T perimetre (void);
+
+    virtual T surface (void);
+
+
+    // Affichage //
+    friend std::ostream &operator<< (std::ostream &s, TForme const &centre){
+        s << "centre : " << this->centre << "\n";
+    return s
+    }
+
+};
+
+
+#endif
